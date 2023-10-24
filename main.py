96da607d10e6c3d2e6fede34ad155a96e7012638
S0IG0 <chibitok.d.s@edu.mirea.ru>
2023-10-24 18:11:45 +0300

    add moving average

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from multiprocessing import Process
 
+from db.models.entity import Sensor
 from db.settings.connect import db
 from db.settings.init import init_db
 from mqtt.client import parse_values
 from plot.animate import animate_line_plot
 from plot.generate import generate_all_plots
+import matplotlib
+
+matplotlib.use('TkAgg')
 
 
 def main():
@@ -19,8 +23,10 @@ def main():
 
 
 if __name__ == '__main__':
+    Sensor.drop_table()
     try:
         init_db()
         main()
     except KeyboardInterrupt:
         pass
+
--- plot/animate.py
+++ plot/animate.py
@@ -1,10 +1,15 @@
 from time import sleep, time
 
 from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
 from numpy import (
     max,
     min,
     mean,
+    arange,
+    linspace,
+    convolve,
+    ones,
 )
 
 from db.models.entity import Sensor, Type
@@ -12,6 +17,8 @@ from db.settings.defualt import DefaultType
 
 
 def animate_line_plot():
+    interp = 500
+    window_size = 5  # Размер окна скользящей средней
     type_id = Type.get(Type.name == DefaultType.temperature)
     plt.ion()
 
@@ -19,16 +26,31 @@ def animate_line_plot():
     while end_time - time() >= 0:
         data = list(map(lambda x: float(x.value), Sensor.select().where(Sensor.type == type_id)))
         if len(data) > 0:
+            x = linspace(0, len(data) - 1, interp)
+            f = interp1d(arange(len(data)), data)
+            data_interpolated = f(linspace(0, len(data) - 1, interp))
+
+            moving_avg = convolve(data, ones(window_size) / window_size, mode='valid')
+            x_moving_avg = linspace(0, len(data) - window_size, len(moving_avg))
+
             plt.clf()
-            plt.plot(data)
+            plt.plot(x, data_interpolated, label="Интерполированные данные")
+            plt.plot(
+                x_moving_avg,
+                moving_avg,
+                label=f'Скользящее среднее ({window_size}-точечное окно)',
+                color='orange'
+            )
             plt.ylabel("Температура")
             plt.axhline(value := min(data), color='red', linestyle='--', label=f'Min: {value:.4}')
             plt.axhline(value := max(data), color='green', linestyle='--', label=f'Max: {value:.4}')
             plt.axhline(value := float(mean(data)), color='blue', linestyle='--', label=f'Mean: {value:.4}')
-            plt.legend()
+            plt.legend(loc='upper right')
+            plt.grid()
             plt.title("График температуры")
             plt.draw()
             plt.gcf().canvas.flush_events()
         sleep(1)
+        plt.pause(0.01)
     plt.ioff()
     plt.show()
